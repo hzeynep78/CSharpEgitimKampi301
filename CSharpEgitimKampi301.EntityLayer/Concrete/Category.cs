2676c6248f58f8234fa580de95acb66d5c49ec86
HİLAL ZEYNEP ÇÖRTÜ <200205033@ostimteknik.edu.tr>
2024-11-07 21:44:49 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        /*
+        int x; //field
+        public int y { get; set; } //property
+
+        void Method1() 
+        {
+            int z; //local variable
+        }
+        */
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public virtual  List<Product> Products { get; set; }
+    }
+}
